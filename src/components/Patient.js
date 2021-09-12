b17a270b1b65355079f67ab2a010e8fed5b9ff39
Aryamol A U <mearyamol@gmail.com>
2021-09-12 22:08:52 +0530

    react reading patient

---
+++ src/components/Patient.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Patient extends Component {
+    state={
+        name:"",
+        address:"",
+        admndate:"",
+        mobile:"",
+        emailid:"",
+        roomno:"",
+        division:"",
+        gender:""
+
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
